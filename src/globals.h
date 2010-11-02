92749052d1ce36e187089db83e46458402c28eb3
Daniele Ricci <daniele.athome@gmail.com>
2010-11-03 00:04:45 +0100

    First application skeleton
    Still have to decide which Twitter library and auth method to use
    
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ src/globals.h
@@ -0,0 +1,51 @@
+/*
+ * MicroTwitter
+ * Globals definitions
+ * Copyright (C) 2009-2010 Daniele Ricci <daniele.athome@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __GLOBALS_H
+#define __GLOBALS_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <Elementary.h>
+#include <libintl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mokosuite/utils/remote-config-service.h>
+
+// default log domain
+#undef EINA_LOG_DOMAIN_DEFAULT
+#define EINA_LOG_DOMAIN_DEFAULT _log_dom
+extern int _log_dom;
+
+#ifdef DEBUG
+#define LOG_LEVEL   EINA_LOG_LEVEL_DBG
+#else
+#define LOG_LEVEL   EINA_LOG_LEVEL_INFO
+#endif
+
+#define MICROTWITTER_DATADIR        DATADIR "/microtwitter"
+
+extern RemoteConfigService* home_config;
+
+void get_screen_size(int *w, int *h);
+
+#endif  /* __GLOBALS_H */
---
+++ src/main.c
@@ -0,0 +1,83 @@
+/*
+ * MicroTwitter
+ * Entry point
+ * Copyright (C) 2009-2010 Daniele Ricci <daniele.athome@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <mokosuite/utils/utils.h>
+#include <mokosuite/utils/remote-config-service.h>
+#include <mokosuite/utils/dbus.h>
+#include <mokosuite/ui/gui.h>
+#include <glib.h>
+#include <dbus/dbus-glib-bindings.h>
+
+#include "globals.h"
+
+#define MICROTWITTER_NAME               "org.mokosuite.twitter"
+#define MICROTWITTER_CONFIG_PATH        MOKOSUITE_DBUS_PATH "/Twitter/Config"
+
+
+// default log domain
+int _log_dom = -1;
+
+RemoteConfigService* home_config = NULL;
+
+
+int main(int argc, char* argv[])
+{
+    // TODO initialize Intl
+    setlocale(LC_ALL, "");
+    bindtextdomain(PACKAGE, LOCALEDIR);
+    textdomain(PACKAGE);
+
+    // initialize log
+    eina_init();
+    _log_dom = eina_log_domain_register(PACKAGE, EINA_COLOR_CYAN);
+    eina_log_domain_level_set(PACKAGE, LOG_LEVEL);
+
+    EINA_LOG_INFO("%s version %s", PACKAGE_NAME, VERSION);
+
+    /* other things */
+    mokosuite_utils_init();
+    mokosuite_utils_glib_init(TRUE);
+    mokosuite_ui_init(argc, argv);
+
+    EINA_LOG_DBG("Loading data from %s", MICROTWITTER_DATADIR);
+
+    // dbus name
+    DBusGConnection* session_bus = dbus_session_bus();
+    if (!session_bus || !dbus_request_name(session_bus, MICROTWITTER_NAME))
+        return EXIT_FAILURE;
+
+    // config database
+    char* cfg_file = g_strdup_printf("%s/.config/%s/settings.conf", g_get_home_dir(), PACKAGE);
+    home_config = remote_config_service_new(session_bus,
+        MICROTWITTER_CONFIG_PATH,
+        cfg_file);
+    g_free(cfg_file);
+
+
+    elm_theme_extension_add(NULL, MICROTWITTER_DATADIR "/theme.edj");
+    // TODO additional themes
+
+    // TODO public timeline or last open window?
+
+    elm_run();
+    elm_shutdown();
+
+    return EXIT_SUCCESS;
+}
